a3ceed76f7a911d143555b0bc4620077b39880b0
Maurice Smorenberg <mauricesmorenberg@mac.home>
2025-05-27 15:39:45 +0200

    First Groq response

---
+++ app/api/groq/main.ts
@@ -0,0 +1,22 @@
+'use server'
+import Groq from "groq-sdk";
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+export async function main() {
+  const chatCompletion = await getGroqChatCompletion();
+  
+  console.log(chatCompletion.choices[0]?.message?.content || "");
+}
+
+export async function getGroqChatCompletion() {
+  return groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: "Say Hello!",
+      },
+    ],
+    model: "llama-3.3-70b-versatile",
+  });
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+'use client'
 import Image from "next/image";
+
+import { main } from "./api/groq/main";
 
 export default function Home() {
   return (
     <main>
       <h2>Dashboard</h2>
+      <button onClick={() => main()}>Say something..</button>
     </main>
   );
 }
